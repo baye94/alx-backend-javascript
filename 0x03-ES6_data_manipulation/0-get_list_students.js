517eb16346a8af1f21623c3b5b7889caeb2c2bd1
baye94 <fallcheikh893@gmail.com>
2023-01-03 16:19:05 +0000

    fix bugs

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
-function getListStudents() {
-  return [
-  {id: 1, firstName: "Guillaume", location: "San Francisco"},
-  {id: 2, firstName: "James", location: "Columbia"},
-  {id: 5, firstName: "Serena", location: "San Francisco"}
-  ];
+export default function getListStudents() {
+    const array = [];
+    array.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+    array.push({ id: 2, firstName: 'James', location: 'Columbia' });
+    array.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
+    return array;
   }
