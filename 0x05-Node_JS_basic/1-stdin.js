882728d55fabf1b24320f67ae734a476d7bd9682
baye94 <fallcheikh893@gmail.com>
2023-02-23 21:13:45 +0000

    fix bugs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
-    }
+  }
 });
 
 process.stdin.on('end', () => {
